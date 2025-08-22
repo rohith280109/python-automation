3b4e454722feccb5563a45b53abdea0fdfaa6c1c
Muppalla SaiRohit <Muppalla.SaiRohit@xoriant.com>
2025-08-22 13:24:39 +0530

    removed folder

--- PythonProject/loggs/multilog3.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-logger = logging.getLogger("multilog3")
-
-def multilogg3():
-    logger.info('hello world')
-    logger.critical("critical error")
-    logger.error("this from multilog3")
